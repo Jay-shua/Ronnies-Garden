3ce84794a83e4ef1ad4eaf70e85ddf51648bcbba
jay-shua <jayshua.designs@gmail.com>
2025-08-08 15:53:29 +0100

    hope with me

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  root: "./",
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
+        rooms: resolve(__dirname, "rooms.html"),
+        gallery: resolve(__dirname, "gallery.html"),
+      },
+    },
   },
 });
